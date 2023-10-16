b2ea23e8f5b938238b2d599d844f909b50dde94b
mlundberg93 <Marcusluundbergh@gmail.com>
2023-10-16 14:49:50 +0200

    Add project files.

---
+++ D20 Grupp 1/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace D20_Grupp_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hej och välkommen till adresslistan.");
+            Console.WriteLine("Skriv 'hjälp' för hjälp!");
+            string command;
+            do
+            {
+                Console.Write("Kommando: ");
+                command = Console.ReadLine();
+                if (command == "hjälp")
+                {
+                    Console.WriteLine($"Tyvärr ej implementerat!");
+                }
+                else if (command == "sluta")
+                {
+
+                }
+                else
+                {
+                    Console.WriteLine($"Okänt kommando: {command}");
+                }
+            } while (command != "sluta");
+            Console.WriteLine("Hej då!");
+        }
+    }
+}
