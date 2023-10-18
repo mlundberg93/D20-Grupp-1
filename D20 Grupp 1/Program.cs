6ce1a42e5b9e79accc2541f2c9cd5270ae3e835c
mlundberg93 <Marcusluundbergh@gmail.com>
2023-10-18 15:22:10 +0200

    Lagt till Spar funktion, en funktion för att ta bort användare

--- D20 Grupp 1/Program.cs
+++ D20 Grupp 1/Program.cs
@@ -7,6 +7,7 @@ namespace D20_Grupp_1
     {
         static void Main(string[] args)
         {
+            
             Console.WriteLine("Hello and welcome to the Yellow Pages!");
             Console.WriteLine("If you need help, type help!");
             string command;
@@ -16,7 +17,7 @@ namespace D20_Grupp_1
                 command = Console.ReadLine();
                 if (command.ToLower() == "Help".ToLower())
                 {
-                    Console.WriteLine($"You can choose between these commandos: \n Load \n Add \n Save \n Delete \n Exit");
+                    Console.WriteLine($"You can choose between these commandos: \n Load \n Add \n List \n Save \n Remove \n Exit");
                 }
                 else if (command.ToLower() == "Exit".ToLower())
                 {
@@ -29,9 +30,9 @@ namespace D20_Grupp_1
                     {
                         string[] splitLine = s.Split(',');
                         phoneList.Add(new Person(splitLine[0], splitLine[1], splitLine[2]));
-                        //Console.WriteLine($"Name: {splitLine[0]} \nPhone: {splitLine[1]} \nAdress: {splitLine[2]}\n");
+
                     }
-                    
+
                 }
                 else if (command.ToLower() == "List".ToLower())
                 {
@@ -39,6 +40,42 @@ namespace D20_Grupp_1
                     foreach (Person p in phoneList)
                     {
                         p.Print();
+                    }
+                }
+                else if (command.ToLower() == "Add".ToLower())
+                {
+                    Console.WriteLine($"Make an addition to the Yellow Pages!\n" +
+                        $"Enter your details here\nSeparated by a comma, e.g Marcus Lundberg,112,Lilla gatan 1: ");
+                    string userInput = Console.ReadLine();
+                    string[] stringParts = userInput.Split(',');
+                    phoneList.Add(new Person(stringParts[0], stringParts[1], stringParts[2]));
+                    Console.WriteLine($"Thank you for your contribution!");
+
+                }
+                else if (command.ToLower() == "Save".ToLower())
+                {
+                    string newFile = "adresser.txt";
+                    Console.WriteLine($"Saving data to the database!");
+                    using (StreamWriter saveFile = new StreamWriter(newFile))
+                    {
+                        foreach (Person p in phoneList)
+                        {
+                            saveFile.WriteLine($"{p.name},{p.phone},{p.adress}");
+                        }
+                    }
+                }
+                else if (command.ToLower() == "remove")
+                {
+                    Console.WriteLine($"Please enter the name for the person you want to remove!");
+                    string userInput = Console.ReadLine();
+                    for (int i = 0; i < phoneList.Count; i++)
+                    {
+                        if (phoneList[i].name.ToLower() == userInput.ToLower())
+                        {
+                            phoneList.RemoveAt(i);
+                            Console.WriteLine($"{userInput} has been removed!");
+                            break;
+                        }
                     }
                 }
                 else
@@ -66,6 +103,7 @@ namespace D20_Grupp_1
             {
                 Console.WriteLine($"Name: {this.name}\n Phone: {this.phone}\n Adress: {this.adress}\n");
             }
+
 
         }
 
